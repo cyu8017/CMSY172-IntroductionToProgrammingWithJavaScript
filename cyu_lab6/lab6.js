122742ca6776ebd47519fbc1fa93f03e8c31717c
Charlie Yu <cyu8017@gmail.com>
2020-04-18 13:35:22 -0400

    Moved CSS to external file.

--- cyu_lab6/lab6.js
+++ cyu_lab6/lab6.js
@@ -1,53 +1,33 @@
-//Name: Charlie Yu
-//Program Description: The Document Object Model
-//Date: 4/18/2018
-
-// The init function sets the event handler
-// functions for the mouseover and mouseout
-// events.
-function init()
-{  
+// The init function sets the event handler functions for the mouseover and mouseout events.
+function init() {  
     // Use the image with the id of map
     var mapImage = document.getElementById("map");
   
-    //Use the clearImage event
-    //handler function for the
-    //mouseover event.
+    // Use the clearImage event handler function for the mouseover event.
     mapImage.onmouseover = clearImage;
 
-    //Use the blurImage event
-    //handler function for the
-    //mouseout event.
+    // Use the blurImage eventhandler function for the mouseout event.
     mapImage.onmouseout = blurImage;  
 }
 
-// mouseover event handler function
-// Use the image with the id of map
-function clearImage() 
-{  
+// Mouseover event handler function. Use the image with the id of map
+function clearImage() {  
     // Use the image with the id of map
     var image = document.getElementById("map");
   
-    // Use the map.jpg image for 
-    // the image source.
+    // Use the map.jpg image for the image source.
     image.src = "map.jpg";
 }
 
-// mouseout event handler function
-
-function blurImage()
-{  
-
+// Mouseout event handler function
+function blurImage() {  
     // Use the image with the id of map
     var image = document.getElementById("map");
   
-    // Use the mapblur.jpg image for 
-    // the image source.
+    // Use the mapblur.jpg image for the image source.
     image.src = "mapblur.jpg";
 }
 
-// Use the init function for the 
-// onload event handler to make 
-// sure the whole page is loaded 
-// before executing any JavaScript..
+// Use the init function for the onload event handler to make sure the whole page is loaded 
+// before executing any JavaScript.
 window.onload = init;
